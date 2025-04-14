7c28180f20295eeb94955a458bcbe8cf8bd84a2f
Mariya Khrustaleva <mbotmanova@gmail.com>
2025-04-14 23:07:32 +0500

    works with feedback form

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -6,4 +7,8 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        request.POST.get('message')
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'catalog/contacts.html')
