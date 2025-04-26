d7c74c76905852535c791f52b45fe80db5b4dc57
Mariya Khrustaleva <mbotmanova@gmail.com>
2025-04-26 21:03:51 +0500

    add fixtures and custom command

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,12 +5,21 @@ from .models import Product, Category
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
-    list_filter = ('category',)
-    search_fields = ('name', 'description',)
+    list_display = ("id", "name", "price", "category")
+    list_filter = ("category",)
+    search_fields = (
+        "name",
+        "description",
+    )
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name',)
-    search_fields = ('name', 'description',)
+    list_display = (
+        "id",
+        "name",
+    )
+    search_fields = (
+        "name",
+        "description",
+    )
