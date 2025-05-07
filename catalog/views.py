11bd903bfe45fda9eca6be6db3e474f3180c6a38
Mariya Khrustaleva <mbotmanova@gmail.com>
2025-05-07 23:45:34 +0500

    add feature add_product

--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,14 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from catalog.models import Product, Contacts
+from catalog.models import Product, Contacts, ProductForm
 
 
 # Create your views here.
 def home(request):
-    all_products = Product.objects.order_by('-id')
+    all_products = Product.objects.order_by("-id")
     paginator = Paginator(all_products, 3)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
     last_products = Product.objects.order_by("-id")[:5]
     print("Последние 5 созданных продуктов:")
@@ -34,3 +34,15 @@ def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {"product": product}
     return render(request, "catalog/product_detail.html", context)
+
+
+def add_product(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            # Перенаправление на страницу товара по его pk
+            return redirect("catalog:product_detail", pk=product.pk)
+    else:
+        form = ProductForm()
+    return render(request, "catalog/add_product.html", {"form": form})
